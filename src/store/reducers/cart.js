524979d6d35f78de3a40d88ec4e27fe860746c3d
Sakib Ahmed <sakibhasan94470@gmail.com>
2024-02-26 08:55:54 +0600

    Update cart.js

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,39 +1,4 @@
-// export const cartReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case "cart/addToCart": {
-//       const product = state.find((item) => item.id === action.payload.id);
-//       return product
-//         ? state.map((item) => {
-//             if (item.id === action.payload.id) {
-//               return { ...item, quantity: item.quantity + 1 };
-//             }
-//             return item;
-//           })
-//         : [...state, { ...action.payload, quantity: 1 }];
-//     }
 
-//     case "cart/removeFromCart": {
-//       return state.filter((item) => item.id !== action.payload);
-//     }
-
-//     case "cart/modifyQunatityOfItem": {
-//       return state.map((item) => {
-//         if (item.id === action.payload.id) {
-//           return {
-//             ...item,
-//             quantity: action.payload.quantity,
-//           };
-//         }
-//         return item;
-//       });
-//     }
-//     case "cart/clearCart": {
-//       return [];
-//     }
-//     default:
-//       return state;
-//   }
-// };
 
 // using  reactToolkit
 import { createReducer } from "@reduxjs/toolkit";
