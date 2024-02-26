8cfecf4eef2fb16d23f9e439e12210185185d8cc
Sakib Ahmed <sakibhasan94470@gmail.com>
2024-02-26 09:08:39 +0600

    shoping Cart using CreateSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,3 @@
-// import { composeWithDevTools } from "@redux-devtools/extension";
-// import { combineReducers, createStore } from "redux";
-// import { cartReducer } from "./reducers/cart";
-// import { counterReducer } from "./reducers/counter";
-// import { themeReducer } from "./reducers/theme";
-
-// const rootReducer = combineReducers({
-//   counter: counterReducer,
-//   theme: themeReducer,
-//   cart: cartReducer,
-// });
-// export const store = createStore(rootReducer, composeWithDevTools());
-
 // using reduc-toolkit
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./reducers/cart";
